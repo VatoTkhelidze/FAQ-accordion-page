7870e2aee850dfd1626c4958d9aa2e7e67dd1906
VatoTkhelidze <vatotxelidze@gmail.com>
2022-08-09 03:49:20 +0400

    added hovers on question lines & when user clicks on question it goes bold and shows answer under it

---
+++ task13.js
@@ -0,0 +1,33 @@
+let questions = document.querySelectorAll('.accordion');
+let answer = document.querySelectorAll('.answers');
+let arrowDown = document.querySelectorAll('.arrow');
+let arrowUp = document.querySelectorAll('.arrow_active');
+
+
+
+for(let i=0; i<questions.length; i++){
+    questions[i].addEventListener('click', function(){
+        for(let j=0; j<answer.length; j++){
+            answer[j].style.display = 'none';
+        }
+        answer[i].style.display = 'block';
+
+
+        for(let l=0; l<arrowDown.length; l++){
+            arrowDown[l].style.display = 'block';
+        }
+        arrowDown[i].style.display = 'none';
+
+
+        for(let k=0; k<arrowUp.length; k++){
+            arrowUp[k].style.display = 'none';
+        }
+        arrowUp[i].style.display = 'block';
+
+
+        for(let m=0; m<questions.length; m++){
+            questions[m].style.fontWeight = '400';
+        }
+        questions[i].style.fontWeight = '700';
+    })
+}
